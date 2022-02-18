1e17bece430d30919c7b323678c40580202565d4
thddlmy <thddlmy@gmail.com>
2022-02-19 01:33:13 +0900

    feat: 업로드 컴포넌트 구현 및 json 파싱 후 객체화 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-export default function App({ $target}) {
-  const $uploadContainer = document.createElement('div');
-  const $treeContainer = document.createElement('div');
+import UploadContainer from './UploadContainer.js';
 
-  $target.appendChild($uploadContainer);
-  $target.appendChild($treeContainer);
+export default function App({ $target }) {
+  const $uploadSection = document.createElement('div');
+  $target.appendChild($uploadSection);
 
+  this.state = {
+    file: null,
+  };
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+  };
+
+  const uploadContainer = new UploadContainer({
+    $target: $uploadSection,
+    initialState: {},
+    onChange: (file) => {},
+  });
 }
---
+++ src/UploadContainer.js
@@ -0,0 +1,50 @@
+export default function UploadContainer({ $target, initialState, onChange }) {
+  const $uploadContainer = document.createElement('div');
+  $target.appendChild($uploadContainer);
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    $uploadContainer.innerHTML = `
+      <label id="input-label" for="input-file">
+        Upload JSON
+      </label>
+      <input type="file" id="input-file" accept=".json" style="display:none;">
+    `;
+  };
+
+  this.render();
+
+  $uploadContainer.addEventListener('change', (e) => {
+    const { files } = e.target;
+    const fileReader = new FileReader();
+    fileReader.readAsText(files[0]);
+    fileReader.onload = () => {
+      console.log(fileReader.result);
+      console.log(makeJSON(JSON.parse(fileReader.result)));
+    };
+  });
+}
+
+function makeJSON(fileObj) {
+  const jsonObj = {};
+  for (const [key, value] of Object.entries(fileObj)) {
+    const split_list = key.split('.');
+    let temp = jsonObj;
+    for (const [index, property] of split_list.entries()) {
+      if (index === split_list.length - 1) {
+        temp[property] = value;
+      } else {
+        if (!temp[property]) {
+          temp[property] = {};
+        }
+        temp = temp[property];
+      }
+    }
+  }
+  return jsonObj;
+}
